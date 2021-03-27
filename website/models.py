5ba344d321e10501eeba9f5fde8785800383cea3
Aldo Munaretto <aldo.munaretto@finect.com>
2021-03-27 21:08:54 +0100

    Database Creation

--- website/models.py
+++ website/models.py
@@ -3,10 +3,10 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class Note(db.Model):
-    id = db.Colum(db.Integer,primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True),default=func.now())
-    user_id = db.Column(db.Integer,db.ForeingKey('user.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key=True)
