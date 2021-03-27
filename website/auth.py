9685f1c241566096cc65772f16d24fdd15bb7e00
Aldo Munaretto <aldo.munaretto@finect.com>
2021-03-27 20:20:35 +0100

    Add message flashing to sign-up page

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth',__name__)
@@ -16,4 +16,24 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        lastName = request.form.get("lastName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 5:
+            flash("Email must be greater than 4 characters", category="error")
+        elif len(firstName) or len(lastName) < 2:
+            flash("First & last name must be greater than 1 character", category="error")
+        elif password1 != password2:
+            flash("Passwords don\'t match", category="error")
+        elif len(password1) < 8:
+            flash("Password must be greater at least 8 characters", category="error")
+        else:
+            flash("ACcount created!",category="success")
+            pass
+            # add user to the database
+
     return render_template("sign_up.html")
