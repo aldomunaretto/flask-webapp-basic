759fb7c60d3656486f16c688bdd3303b4a505102
Aldo Munaretto <aldo.munaretto@finect.com>
2021-03-27 23:48:50 +0100

    Use current_user in templates

--- website/auth.py
+++ website/auth.py
@@ -26,7 +26,7 @@ def login():
 
 
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
@@ -65,4 +65,4 @@ def sign_up():
             flash("Account created!",category="success")
             return redirect(url_for("views.home"))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
