9e19262cd8413e7db589d73642db8bd0643bc5bf
Aldo Munaretto <aldo.munaretto@finect.com>
2021-03-28 00:42:05 +0100

    Fix indentation

--- website/views.py
+++ website/views.py
@@ -13,13 +13,13 @@ def home():
         note = request.form.get('note')
 
     
-    if len(note) < 1:
-        flash("Note is empty", category="error")
-    else:
-        new_note = Note(data=note, user_id=current_user.id)
-        db.session.add(new_note)
-        db.session.commit()
-        flash("Note added", category="success")
+        if len(note) < 1:
+            flash("Note is empty", category="error")
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note added", category="success")
 
 
     return render_template("home.html", user=current_user)
